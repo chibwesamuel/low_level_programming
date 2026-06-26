181790ecc43672ca8423126f58102ac0a8659c99
chibwesamuel <smchibwe@yahoo.com>
2026-06-26 11:51:18 +0200

    feat: added first C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: Prints a quote using the puts function.
+ * 
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
